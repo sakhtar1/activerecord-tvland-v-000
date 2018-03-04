578fe3395353c415c239d38dab46f6a63ff82d61
Sharmina Akhtar <sakht318@gmail.com>
2018-03-04 17:21:00 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    actor = Actor.create(:first_name => first_name, :last_name => last_name)
+    "#{self.first_name}  #{self.last_name}"
     actor.save
     actor
   end
