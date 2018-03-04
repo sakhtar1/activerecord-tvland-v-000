ef05134a6208618869cf1717e47d0a72ac62fe8e
Sharmina Akhtar <sakht318@gmail.com>
2018-03-04 16:57:29 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = first_name + last_name
+    return full_name
+  end
+
+  def list_roles
+
+  end
+
 end
