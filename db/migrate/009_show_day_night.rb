afafc962ae45b4cb8f974719d800ea9f45c28c9e
Sharmina Akhtar <sakht318@gmail.com>
2018-03-04 17:27:29 +0000

    automatically backed up by learn

--- db/migrate/009_show_day_night.rb
+++ db/migrate/009_show_day_night.rb
@@ -1,4 +1,4 @@
-class CreateShowDayNight < ActiveRecord::Migration[4.2]
+class ShowDayNight < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
     dd_column :shows, :season, :string
